2e662fbb12eafb0f3e5ecdc7a69a285e94cc4764
Jules AZEMAR <juazemar@iutclinfa1011m.local>
2018-01-19 08:06:11 +0100

    Initial Commit

---
+++ BatailleNavale/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BatailleNavale
+//
+//  Created by Jules AZEMAR on 19/01/2018.
+//  Copyright © 2018 Jules AZEMAR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
